214468bb4c0416ba8b487b80bb35548f86007762
funzter <cavehive@protonmail.com>
2024-05-17 21:41:53 +0200

    minor

--- src/main/java/de/timesnake/game/survival/chat/Plugin.java
+++ src/main/java/de/timesnake/game/survival/chat/Plugin.java
@@ -5,21 +5,13 @@
 package de.timesnake.game.survival.chat;
 
 
-import de.timesnake.library.basic.util.LogHelper;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-  public static final Plugin SURVIVAL = new Plugin("Survival", "GSS",
-      LogHelper.getLogger("Survival", Level.INFO));
-  public static final Plugin MACHINES = new Plugin("Machine", "GSM",
-      LogHelper.getLogger("Machine", Level.INFO));
-  public static final Plugin REWARDS = new Plugin("Rewards", "GSR",
-      LogHelper.getLogger("Rewards", Level.INFO));
+  public static final Plugin SURVIVAL = new Plugin("Survival", "GSS");
+  public static final Plugin MACHINES = new Plugin("Machine", "GSM");
+  public static final Plugin REWARDS = new Plugin("Rewards", "GSR");
 
-  protected Plugin(String name, String code, Logger logger) {
+  protected Plugin(String name, String code) {
     super(name, code);
   }
 
